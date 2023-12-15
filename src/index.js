035ef2f2bc4773a5e5f3cb414806fbbeffae8b29
OlenaKovalenko <elenkow17@gmail.com>
2023-12-15 16:17:33 +0200

    Add pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from 'components/App/App';
 import { ThemeProvider } from 'styled-components';
@@ -10,9 +12,13 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
