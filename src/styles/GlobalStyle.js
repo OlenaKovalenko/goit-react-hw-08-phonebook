40f21c681611b0fc4552e5d15fadc3eabf24d29f
OlenaKovalenko <elenkow17@gmail.com>
2023-12-17 21:20:38 +0200

    Update GlobalStyle.js

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,8 +5,8 @@ export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-          'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-          sans-serif;
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         background-size: cover;
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
 
     code {
         font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-          monospace;
+            monospace;
     }
 
     h1,
